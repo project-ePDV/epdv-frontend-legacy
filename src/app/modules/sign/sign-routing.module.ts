f82bbae8d5cb55e826639447ea3d2e98516b5378
Kal Nascimento <kalebe.nascimento19@gmail.com>
2023-03-18 12:32:35 -0300

    create register page

--- src/app/modules/sign/sign-routing.module.ts
+++ src/app/modules/sign/sign-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPage } from './pages/login/login.page';
+import { RegisterPage } from './pages/register/register.page';
 
 const routes: Routes = [
 	{
 		path: 'login',
 		component: LoginPage,
+		pathMatch: 'full'
+	},
+	{
+		path: '',
+		component: RegisterPage,
 		pathMatch: 'full'
 	}
 ];
--- src/app/modules/sign/sign.module.ts
+++ src/app/modules/sign/sign.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { SignRoutingModule } from './sign-routing.module';
 import { LoginPage } from './pages/login/login.page';
 import { IonicModule } from '@ionic/angular';
+import { RegisterPage } from './pages/register/register.page';
 
 @NgModule({
-  declarations: [LoginPage],
+  declarations: [LoginPage, RegisterPage],
   imports: [
     SignRoutingModule,
     IonicModule.forRoot()
