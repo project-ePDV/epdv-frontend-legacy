877d6a0ab71391d3bc35663b1c3ff42da096158f
Kal Nascimento <kalebe.nascimento19@gmail.com>
2023-06-03 18:17:17 -0300

    feat: create pagination component

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -4,10 +4,24 @@ import { HeaderComponent } from './components/header/header.component';
 import { IonicModule } from '@ionic/angular';
 import { MenuTabComponent } from './components/menu-tab/menu-tab.component';
 import { FilterComponent } from './components/filter/filter.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
 
 @NgModule({
-  declarations: [HeaderComponent, MenuTabComponent, FilterComponent],
-  imports: [CommonModule, IonicModule.forRoot()],
-  exports: [HeaderComponent, MenuTabComponent, FilterComponent]
+  declarations: [
+    HeaderComponent,
+    MenuTabComponent, 
+    FilterComponent, 
+    PaginationComponent
+    ],
+  imports: [
+    CommonModule, 
+    IonicModule.forRoot()
+  ],
+  exports: [
+    HeaderComponent, 
+    MenuTabComponent, 
+    FilterComponent,
+    PaginationComponent
+  ]
 })
 export class SharedModule {}
